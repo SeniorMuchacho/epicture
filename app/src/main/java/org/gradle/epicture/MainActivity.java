7ac1895e971bd2681c461a5eb7872caa91e024e3
NOAH <lothaire.noah@epitech.eu>
2018-11-01 15:57:26 +0100

    push all for second time

--- app/src/main/java/org/gradle/epicture/MainActivity.java
+++ app/src/main/java/org/gradle/epicture/MainActivity.java
@@ -2,19 +2,9 @@ package org.gradle.epicture;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Button;
-
-import butterknife.BindView;
-import butterknife.OnClick;
-
 public class MainActivity extends AppCompatActivity implements  View.OnClickListener{
 
 
@@ -23,20 +13,6 @@ public class MainActivity extends AppCompatActivity implements  View.OnClickList
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         findViewById(R.id.connection).setOnClickListener(this);
-
-        /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);*/
-
-     /*   Button button = (Button) findViewById(R.id.toLogin);
-        button.setOnClickListener(new View.OnClickListener()
-        {
-            public void onClick(View v)
-            {
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });*/
     }
 
     @Override
@@ -45,27 +21,4 @@ public class MainActivity extends AppCompatActivity implements  View.OnClickList
         startActivity(intent);
         finish();
     }
-
-    /*@Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }*/
-
 }
